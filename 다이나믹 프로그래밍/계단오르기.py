74ad1ebc9d11c0be1b6c8aec13aedfbfa7177e88
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-13 17:04:09 +0900

    계단을 오르면서 획득할 수 있는 최대 점수 구하는 문제, 메모리 초과 문제 해결

--- 다이나믹 프로그래밍/계단오르기.py
+++ 다이나믹 프로그래밍/계단오르기.py
@@ -1,37 +1,30 @@
 import sys
-
 n = int(sys.stdin.readline())
 stair = [int(sys.stdin.readline()) for i in range(n)]
 dp = [[] for i in range(n)]
-dp[0].append([0])
+dp[0].append([1, stair[0]])
 if n > 1:
-    dp[1].append([0, 1])
-    dp[1].append([1])
+    temp = stair[0] + stair[1]
+    dp[1].append([1, temp])
+    dp[1].append([2, stair[1]])
 for i in range(2, n):
     for j in dp[i - 1]:
-        if len(j) >= 2:
-            if j[-1] != i - 1 or j[-2] != i - 2:
-                temp = j[:]
-                temp.append(i)
-                dp[i].append(temp)
-        else:
-            temp = j[:]
-            temp.append(i)
-            dp[i].append(temp)
+        if j[0] != 1:
+            temp = j[1] + stair[i]
+            dp[i].append([1, temp])
+    tempMax = None
     for j in dp[i - 2]:
-        temp = j[:]
-        temp.append(i)
-        dp[i].append(temp)
+        if tempMax == None:
+            tempMax = j[1]
+        elif tempMax < j[1]:
+            tempMax = j[1]
+    tempMax += stair[i]
+    dp[i].append([2, tempMax])
 maxSum = None
 for i in dp[n - 1]:
-    tempSum = 0
-    for j in i:
-        tempSum += stair[j]
     if maxSum == None:
-        maxSum = tempSum
-    elif tempSum > maxSum:
-        maxSum = tempSum
-
+        maxSum = i[1]
+    elif i[1] > maxSum:
+        maxSum = i[1]
+    # print(dp)
 print(maxSum)
-
-
