2c11ac20ff01ba91870a236c77ada404b3f6f523
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-21 22:03:57 +0900

    graham scan 이용 문제, 시간초과 해결, 오답 테스트 케이스 전부 통과

--- 기하학/볼록 껍질.py
+++ 기하학/볼록 껍질.py
@@ -15,10 +15,12 @@ for i in range(n):
 
 
 def calcangle(stan, compare):
+    if stan == compare:
+        return [-1, -1]
     dx = compare[0] - stan[0]
     dy = compare[1] - stan[1]
     if dx >= 0 and dy == 0:
-        return 0
+        return [0, (dx ** 2 + dy ** 2) ** (1 / 2)]
     else:
         angle = abs(dy) / (abs(dx) + abs(dy))
         if dx < 0 and dy >= 0:
@@ -27,7 +29,7 @@ def calcangle(stan, compare):
             angle = 2 + angle
         elif dx > 0 and dy < 0:
             angle = 3 + angle
-        return angle * 90
+        return [angle * 90, (dx ** 2 + dy ** 2) ** (1 / 2)]
 
 
 points.sort(key=lambda points: calcangle(minY, points))
@@ -40,19 +42,22 @@ def ccw(a, b, c):
 # 양수면 CCW
 
 jmarch = [points[0], points[1], points[2]]
-if ccw(jmarch[0],jmarch[1],jmarch[2]) == 0:
-    jmarch.pop(1)
+
 for i in range(3, n):
     while True:
         check = ccw(jmarch[-2], jmarch[-1], points[i])
-        if check == 0:
-            jmarch.pop()
-            jmarch.append(points[i])
-            break
-        elif check > 0:
+        if check >= 0:
             jmarch.append(points[i])
             break
         else:
             jmarch.pop()
-print(len(jmarch))
+result = 0
+for i in range(1, len(jmarch) - 1):
+    if ccw(jmarch[i - 1], jmarch[i], jmarch[i + 1]) > 0:
+        result += 1
+if ccw(jmarch[-2], jmarch[-1], jmarch[0]) > 0:
+    result += 1
+if ccw(jmarch[-1], jmarch[0], jmarch[1]) > 0:
+    result += 1
+print(result)
 
