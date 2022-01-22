53ec1f56aaf119c4080177712c9a234bfe33670a
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-23 00:05:11 +0900

    단어를 숫자로 대체해서 더할 때 가장 큰 합 구하기

---
+++ 그리디/단어 수학.py
@@ -0,0 +1,22 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+alpha = [0 for i in range(26)]
+for i in range(n):
+    query = sys.stdin.readline().strip()
+    for j in range(len(query)):
+        alpha[ord(query[j]) - 65] += 10 ** (len(query) - 1 - j)
+
+alpha.sort()
+alpha.reverse()
+v = 9
+result = 0
+for i in range(26):
+    if alpha[i]:
+
+        result += v * alpha[i]
+        v -= 1
+    else:
+        break
+print(result)
