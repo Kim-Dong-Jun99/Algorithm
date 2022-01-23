4369d479d17d4450bf5443c50d07b5272a60881f
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-23 19:20:52 +0900

    KMP 알고리즘 문제, 시간 개선

--- 문자열/찾기.py
+++ 문자열/찾기.py
@@ -1,35 +1,32 @@
 import sys
+
 n = sys.stdin.readline().strip()
 pattern = sys.stdin.readline().strip()
-kmp = [0 for i in range(len(pattern)+1)]
+kmp = [0 for i in range(len(pattern) + 1)]
 kmp[0] = -1
-for i in range(1,len(pattern)+1):
-    if i == 1:
-        kmp[i] = 0
+kmp[1] = 0
+for i in range(1, len(kmp)-1):
+    if pattern[kmp[i]] == pattern[i]:
+        kmp[i+1] = kmp[i]+1
     else:
-        index = (i)//2
-        while index > 0:
-            if pattern[:index] == pattern[i-index:i]:
-                break
-            else:
-                index -= 1
-        kmp[i] = index
-
+        if pattern[i] == pattern[0]:
+            kmp[i+1] = 1
+        else:
+            kmp[i+1] = 0
 
 result = []
 i = 0
-while i+len(pattern)-1 < len(n):
+while i + len(pattern) - 1 < len(n):
     j = 0
-    while j < len(kmp):
-        if j == len(kmp)-1:
-            result.append(str(i+1))
-            i += j-kmp[j]
-            break
+    while j < len(kmp) - 1:
+        if n[i + j] == pattern[j]:
+            j += 1
         else:
-            if n[i+j] == pattern[j]:
-                j += 1
-            else:
-                i += j-kmp[j]
-                break
+            i += j - kmp[j]
+            break
+    if j == len(kmp) - 1:
+        result.append(str(i + 1))
+        i += j - kmp[j]
+
 print(len(result))
 print(' '.join(result))
