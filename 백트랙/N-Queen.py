c0993842da9e194a6e223f62fa9c14aa77481d8d
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-25 10:13:39 +0900

    공부해서 구현해본 백트랙 코드, 시간 초과 문제 발생

--- 백트랙/N-Queen.py
+++ 백트랙/N-Queen.py
@@ -1,78 +1,23 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
+result = 0
+cb = [0 for _ in range(n)]
 
+def check(m):
+    for i in range(m):
+        if cb[m] == cb[i] or abs(cb[i]-cb[m]) == m-i:
+            return False
+    return True
 
-def checkps(i, j, a, b):
-    if i == a:
-        return False
-    elif j == b:
-        return False
-    elif abs(i - a) == abs(j - b):
-        return False
-    else:
-        return True
+def backtrack(v):
+    global result
+    if v == n:
+        result += 1
+        return
+    for i in range(n):
+        cb[v] = i
+        if check(v):
+            backtrack(v+1)
 
-
-class Node():
-    def __init__(self, data):
-        self.data = data
-        self.child = None
-        self.visited = [0 for _ in range(n)]
-
-
-class queens():
-    def __init__(self):
-        self.head = Node((-1, -1))
-        self.head.child = [Node((0, i)) for i in range(n)]
-        self.table = {}
-        for i in range(n):
-            for j in range(n):
-                self.table[(i, j)] = Node((i, j))
-                if i + 1 < n:
-                    self.table[(i, j)].child = [Node((i + 1, k)) for k in range(n)]
-
-    def backtrack(self):
-        result = 0
-        cur = self.head
-        stack = []
-        while True:
-            print('cur %s' % list(cur.data))
-            if cur.child == None:
-                print('!!!!!')
-                result += 1
-                cur = stack.pop()
-            else:
-                f = False
-                for i in cur.child:
-                    # print(list(i.data))
-                    if cur.visited[i.data[1]] == 0:
-                        if cur.data != (-1, -1):
-                            if checkps(cur.data[0], cur.data[1], i.data[0], i.data[1]):
-                                check = True
-                                for j in stack:
-                                    if j.data != None:
-                                        if checkps(j.data[0], j.data[1], i.data[0], i.data[1]) == False:
-                                            check = False
-                                            break
-                                if check:
-                                    stack.append(cur)
-                                    cur.visited[i.data[1]] = 1
-                                    cur = i
-                                    f = True
-                                    break
-                        else:
-                            stack.append(cur)
-                            cur.visited[i.data[1]] = 1
-                            cur = i
-                            f = True
-                            break
-                if f == False:
-                    if stack:
-                        cur = stack.pop()
-                    else:
-                        break
-
-        return result
-
-
-q = queens()
-print(q.backtrack())
+backtrack(0)
+print(result)
