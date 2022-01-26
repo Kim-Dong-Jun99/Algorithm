c3645b5aea1e75d35a84bd079f0d8df51ec51fcb
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-26 15:19:30 +0900

    개선된 풀이

--- 이분 탐색/공유기 설치.py
+++ 이분 탐색/공유기 설치.py
@@ -3,31 +3,20 @@ import sys
 n, c = map(int, sys.stdin.readline().split())
 homes = [int(sys.stdin.readline()) for _ in range(n)]
 homes.sort()
-div = (homes[-1] - homes[0]) // (c - 1)
-left = 0
-right = len(homes) - 1
-minD = div
-
-for _ in range(c - 2):
-    right = len(homes) - 1
-    find = homes[left] + div
-
-    while True:
-        mid = (left + right) // 2
-        if homes[mid] == find:
-            left = mid
-            break
-        elif homes[mid] < find:
-            left = mid+1
-        else:
-            right = mid-1
-        if right < left:
-            if abs(homes[right] - find) < abs(homes[left] - find):
-                if find - abs(homes[right] - find) < minD:
-                    minD = find - abs(homes[right] - find)
-                left = right
-            else:
-                if find - abs(homes[left] - find) < minD:
-                    minD = find - abs(homes[left] - find)
-            break
-print(minD)
+left = 1
+right = homes[-1] - homes[0]
+result = 0
+while left <= right:
+    temp = homes[0]
+    count = 1
+    mid = (left+right) // 2
+    for i in range(1,n):
+        if temp+mid <= homes[i]:
+            count += 1
+            temp = homes[i]
+    if count >= c:
+        left = mid+1
+        result = mid
+    else:
+        right = mid-1
+print(result)
