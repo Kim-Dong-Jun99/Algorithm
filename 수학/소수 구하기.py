3b60f68274d038009f7aaff3b0cfb77cfefb914c
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-01-28 17:15:16 +0900

    시간 복잡도 개선

--- 수학/소수 구하기.py
+++ 수학/소수 구하기.py
@@ -1,15 +1,13 @@
 import sys
-n = int(sys.stdin.readline())
-ns = list(map(int,sys.stdin.readline().split()))
+n,m = map(int,sys.stdin.readline().split())
 
-isprime = [0 for i in range(1000+1)]
-isprime[1] = 1
-for i in range(2,1001):
-    if isprime[i] == 0:
-        for j in range(2,1000//i+1):
-            isprime[j*i] = 1
-result = 0
-for i in ns:
-    if isprime[i] == 0:
-        result += 1
-print(result)
+for i in range(n,m+1):
+    if i > 1:
+        check = True
+        for j in range(2, int(i**(1/2))+1):
+            if i % j == 0:
+                check = False
+                break
+        if check:
+            print(i)
+
