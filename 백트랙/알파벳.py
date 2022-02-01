d1a10628388e43d7441e413fd9c3031965109f37
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-01 10:32:40 +0900

    겹치지 않은 알파벳들을 최대로 방문하기, 시간초과 메모리 초과 문제 해결

--- 백트랙/알파벳.py
+++ 백트랙/알파벳.py
@@ -1,12 +1,10 @@
 import sys
 
-sys.setrecursionlimit(10 ** 9)
 n, m = map(int, sys.stdin.readline().split())
 graph = [sys.stdin.readline().strip() for _ in range(n)]
-alpha = [0 for _ in range(26)]
+alpha = set()
 maxV = 0
-
-alpha[ord(graph[0][0]) - 65] = 1
+alpha.add(graph[0][0])
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
@@ -18,10 +16,10 @@ def bt(i, j, tempV):
     for k in range(4):
         nx = i + dx[k]
         ny = j + dy[k]
-        if 0 <= nx < n and 0 <= ny < m and alpha[ord(graph[nx][ny]) - 65] == 0:
-            alpha[ord(graph[nx][ny]) - 65] = 1
+        if 0 <= nx < n and 0 <= ny < m and not graph[nx][ny] in alpha:
+            alpha.add(graph[nx][ny])
             bt(nx, ny, tempV + 1)
-            alpha[ord(graph[nx][ny]) - 65] = 0
+            alpha.remove(graph[nx][ny])
 
 
 bt(0, 0, 1)
