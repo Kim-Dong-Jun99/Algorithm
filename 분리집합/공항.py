dc4b1ad357b66761db1d5009880cda7d560e3880
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-07 21:44:24 +0900

    gi 번째 게이트보다 작은 게이트에 비행기를 주차시킬 수 있을 때 주차 가능한 최대 비행기 수 구하기, 시간초과 문제 해결

--- 분리집합/공항.py
+++ 분리집합/공항.py
@@ -2,19 +2,37 @@ import sys
 
 g = int(sys.stdin.readline())
 p = int(sys.stdin.readline())
+parent = {}
+for i in range(g + 1):
+    parent[i] = i
 
-gates = [0 for _ in range(g + 1)]
+planes = [int(sys.stdin.readline()) for _ in range(p)]
+
+
+def find(x):
+    if x == parent[x]:
+        return x
+    else:
+        parent[x] = find(parent[x])
+        return parent[x]
+
+
+def union(x, y):
+    rx = find(x)
+    ry = find(y)
+
+    if rx != ry:
+        parent[rx] = ry
+
+
 count = 0
-for _ in range(p):
-    plane = int(sys.stdin.readline())
-    check = True
-    for i in range(plane, 0, -1):
-        if gates[i] == 0:
-            gates[i] = 1
-            check = False
-            break
-
-    if check:
+for i in planes:
+    # print('----------')
+    # print(parent)
+    temp = find(i)
+    if temp == 0:
         break
     count += 1
+    union(temp,temp-1)
+    # print(parent)
 print(count)
