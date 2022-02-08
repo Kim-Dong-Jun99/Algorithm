1840a9080a04f7614c442ff466b72f99f9dfded4
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-08 19:38:03 +0900

    분리 집합으로 접근해봤는데 시간초과 발생

--- 그래프 이론/백조의 호수.py
+++ 그래프 이론/백조의 호수.py
@@ -1,113 +1,112 @@
 import sys
-from collections import deque
-import time
-start = time.time()
 
-def checkmelt(i,j):
-    if i-1 > -1:
-        if lake[i-1][j] == '.' or lake[i-1][j] == 'L':
-            return True
-    if j-1 > -1:
-        if lake[i][j-1] == '.' or lake[i][j-1] == 'L':
-            return True
-    if i+1 < n:
-        if lake[i+1][j] == '.' or lake[i+1][j] == 'L':
-            return True
-    if j+1 < m:
-        if lake[i][j+1] == '.' or lake[i][j+1] == 'L':
-            return True
-    return False
+sys.setrecursionlimit(10 ** 9)
+n, m = map(int, sys.stdin.readline().split())
+lake = [sys.stdin.readline().strip() for _ in range(n)]
+parent = {}
 
-def meltnext(i,j):
-    result = deque([])
-    if i-1 > -1:
-        if lake[i-1][j] == 'X':
-            result.append([i-1,j])
-    if j-1 > -1:
-        if lake[i][j-1] == 'X':
-            result.append([i,j-1])
-    if i+1 < n:
-        if lake[i+1][j] == 'X':
-            result.append([i+1,j])
-    if j+1 < m:
-        if lake[i][j+1] == 'X':
-            result.append([i,j+1])
-    return result
 
-def cango(i,j):
-    result = deque([])
+def find(x):
+    if x == parent[x]:
+        return x
+    else:
+        parent[x] = find(parent[x])
+        return parent[x]
+
+
+def union(x, y):
+    rx = find(x)
+    ry = find(y)
+    if rx != ry:
+        parent[rx] = ry
+
+
+def findn(i, j):
+    result = []
+    check = False
     if i - 1 > -1:
-        if lake[i - 1][j] == '.' or lake[i-1][j] == 'L':
-            result.append([i - 1, j])
-    if j - 1 > -1:
-        if lake[i][j - 1] == '.' or lake[i][j-1] == 'L':
-            result.append([i, j - 1])
+        if lake[i - 1][j] == '.' or lake[i - 1][j] == 'L':
+            if parent.get((i - 1, j), -1) == -1:
+                result.append((i - 1, j))
+        else:
+            check = True
     if i + 1 < n:
-        if lake[i + 1][j] == '.' or lake[i+1][j] == 'L':
-            result.append([i + 1, j])
+        if lake[i + 1][j] == '.' or lake[i + 1][j] == 'L':
+            if parent.get((i + 1, j), -1) == -1:
+                result.append((i + 1, j))
+        else:
+            check = True
+    if j - 1 > -1:
+        if lake[i][j - 1] == '.' or lake[i][j - 1] == 'L':
+            if parent.get((i, j - 1), -1) == -1:
+                result.append((i, j - 1))
+        else:
+            check = True
     if j + 1 < m:
-        if lake[i][j + 1] == '.' or lake[i][j+1] == 'L':
-            result.append([i, j + 1])
+        if lake[i][j + 1] == '.' or lake[i][j + 1] == 'L':
+            if parent.get((i, j + 1), -1) == -1:
+                result.append((i, j + 1))
+        else:
+            check = True
+    if check:
+        wateredges.append((i, j))
     return result
 
-n,m = map(int,sys.stdin.readline().split())
-lake = [list(sys.stdin.readline().strip()) for i in range(n)]
-melt = deque([])
+
+wateredges = []
+
 swan = []
-days = 0
-nextmelt = [[0 for m_itr in range(m)] for n_itr in range(n)]
-visited = [[0 for i_itr in range(m)] for j_itr in range(n)]
 for i in range(n):
     for j in range(m):
-        if lake[i][j] == 'X':
-            if checkmelt(i,j):
-                melt.append([i,j])
-                nextmelt[i][j] = 1
-        elif lake[i][j] == 'L':
-            swan.append([i,j])
-nextV = deque([swan[0]])
-visited[swan[0][0]][swan[0][1]] = 1
-edge = deque([])
-while nextV:
-    tempEdge = deque([])
-    for i in nextV:
-        check = True
-        for j in cango(i[0],i[1]):
-            if visited[j[0]][j[1]] == 0:
-                visited[j[0]][j[1]] = 1
-                tempEdge.append(j)
-                check = False
-        if check:
-            edge.append(i)
-    nextV = tempEdge
+        if lake[i][j] == 'L':
+            swan.append((i, j))
+        if (lake[i][j] == '.' or lake[i][j] == 'L') and parent.get((i, j), -1) == -1:
 
-while True:
-    if visited[swan[1][0]][swan[1][1]] == 1:
-        print(days)
-        break
-    tempMelt = deque([])
-    for i in melt:
-        lake[i[0]][i[1]] = '.'
-        for j in meltnext(i[0],i[1]):
-            if nextmelt[j[0]][j[1]] == 0:
-                tempMelt.append(j)
-                nextmelt[j[0]][j[1]] = 1
-    melt = tempMelt
-    days += 1
+            parent[(i, j)] = (i, j)
+            nextV = [(i, j)]
+            while nextV:
+                temp = []
+                for k in nextV:
+                    for l in findn(k[0], k[1]):
+                        parent[l] = l
+                        union((i, j), l)
+                        temp.append(l)
 
-    nextEdge = deque([])
-    while edge:
-        temp = deque([])
-        for i in edge:
-            check = True
-            for j in cango(i[0],i[1]):
-                if visited[j[0]][j[1]] == 0:
-                    temp.append(j)
-                    visited[j[0]][j[1]] = 1
-                    check = False
-            if check:
-                nextEdge.append(i)
-        edge = temp
-    edge = nextEdge
+                nextV = temp
 
-print(time.time()-start)
+
+def nextmelt(i, j):
+    result = []
+    if i - 1 > -1:
+        result.append((i - 1, j))
+    if i + 1 < n:
+        result.append((i + 1, j))
+    if j - 1 > -1:
+        result.append((i, j - 1))
+    if j + 1 < m:
+        result.append((i, j + 1))
+    return result
+
+
+count = 0
+while find(swan[0]) != find(swan[1]) and count < 10:
+
+    tempW = []
+    tempI = []
+
+    for i in wateredges:
+        for j in nextmelt(i[0], i[1]):
+            if parent.get(j, -1) == -1:
+                parent[j] = j
+                union(i, j)
+                tempW.append(j)
+                for k in nextmelt(j[0], j[1]):
+                    if parent.get(k, -1) != -1:
+                        union(j, k)
+            else:
+                union(i, j)
+    wateredges = tempW
+
+    count += 1
+
+print(count)
