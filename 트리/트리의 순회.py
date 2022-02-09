616d093006fc64e96abf38eaa4b82f48e8f11bcb
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-09 13:56:07 +0900

    중위 순회, 후위 순회 한 결과로 전위 순회한 결과 구하기, 시간초과

--- 트리/트리의 순회.py
+++ 트리/트리의 순회.py
@@ -1,5 +1,6 @@
 import sys
-sys.setrecursionlimit(10**9)
+
+sys.setrecursionlimit(10 ** 9)
 n = int(sys.stdin.readline())
 
 inorder = list(map(int, sys.stdin.readline().split()))
@@ -7,50 +8,36 @@ postorder = list(map(int, sys.stdin.readline().split()))
 
 
 # head = postorder[-1]
-class Node():
-    def __init__(self, data):
-        self.data = data
-        self.lc = None
-        self.rc = None
-
 
 def getindex(left, right, v):
-    for i in range(left, right+1):
+    for i in range(left, right + 1):
         if inorder[i] == v:
             return i
 
 
-def getfulltree(start,end, left, right):
+result = []
+
+
+def getfulltree(start, end, left, right):
     if left == right:
-        # print('-1-1-1-1-1')
-        return Node(postorder[end])
+        result.append(postorder[end])
+        # print(postorder[end])
+
     else:
-        head = Node(postorder[end])
-        # print('head %d'%postorder[end])
-        # print('%d %d'%(start, end))
-        # print('%d %d'%(left,right))
+        result.append(postorder[end])
+        # print(postorder[end])
+
         headindex = getindex(left, right, postorder[end])
-        # print(headindex)
-        # head.lc = getfulltree(start, left + headindex-1, left, left + headindex)
-        # head.rc = getfulltree(start, right-1, headindex+1, right-1)
+
         ln = headindex - left
         rn = right - headindex
-        if start <= start+ln-1:
-            head.lc = getfulltree(start, start+ln-1, left, headindex-1)
-        if end - rn <= end-1:
-            head.rc = getfulltree(end-rn, end-1, headindex+1, right)
-
-        return head
+        if start <= start + ln - 1:
+            getfulltree(start, start + ln - 1, left, headindex - 1)
+        if end - rn <= end - 1:
+            getfulltree(end - rn, end - 1, headindex + 1, right)
 
 
-root = getfulltree(0, n-1, 0, n-1)
+getfulltree(0, n - 1, 0, n - 1)
+print(*result)
 
 
-def preorder(root):
-    print(root.data,end = ' ')
-    if root.lc != None:
-        preorder(root.lc)
-    if root.rc != None:
-        preorder(root.rc)
-
-preorder(root)
