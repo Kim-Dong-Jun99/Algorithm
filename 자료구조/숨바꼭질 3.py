af3619b24ec921959933151c61d7255f2631d177
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-12 20:59:06 +0900

    숨바꼭질에서 찾는 최소 시간 구하기

---
+++ 자료구조/숨바꼭질 3.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+n, k = map(int, sys.stdin.readline().split())
+
+nextV = deque()
+nextV.append(n)
+visited = [-1] * (10 ** 5 + 1)
+visited[n] = 0
+
+while nextV:
+    cur = nextV.popleft()
+    if cur == k:
+        print(visited[cur])
+        break
+    if cur - 1 >= 0 and visited[cur - 1] == -1:
+        nextV.append(cur - 1)
+        visited[cur - 1] = visited[cur] + 1
+    if cur * 2 <= 10 ** 5 and visited[cur * 2] == -1:
+        nextV.append(cur * 2)
+        visited[cur * 2] = visited[cur]
+
+    if cur + 1 <= 10 ** 5 and visited[cur + 1] == -1:
+        nextV.append(cur + 1)
+        visited[cur + 1] = visited[cur] + 1
+
+
+
+
+
+
