715ac45773fdbda7a18422ae1d4f204863b23513
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-18 14:23:48 +0900

    유럽 여행을 할 때 최소 비용으로 여행할 수 있는 비용 구하기

--- 최소 신장 트리/유럽 여행.py
+++ 최소 신장 트리/유럽 여행.py
@@ -17,65 +17,32 @@ def union(x, y):
         parent[rx] = ry
 
 
-# def dfs(cur):
-#     visited[cur] = 1
-#     global temp
-#     temp += cost[cur]
-#     for i in graph[cur]:
-#         if visited[i[0]] == 0:
-#             temp += i[1]
-#             dfs(i[0])
-#             temp += cost[cur]
-
-
 n, p = map(int, sys.stdin.readline().split())
-
-cost = [0]
+cost = [sys.maxsize]
 for _ in range(n):
     cost.append(int(sys.stdin.readline()))
-
 path = [list(map(int, sys.stdin.readline().split())) for _ in range(p)]
-
 parent = [i for i in range(n + 1)]
-
 mst = []
-path.sort(key=lambda a: a[2] + cost[a[0]] + cost[a[1]])
+path.sort(key=lambda a: a[2]*2 + cost[a[0]] + cost[a[1]])
 for i in path:
     if find(i[0]) != find(i[1]):
         union(i[0], i[1])
         mst.append(i)
-
+        # if len(mst) == n-1:
+        #     break
 graph = [[] for _ in range(n+1)]
 for i in mst:
     graph[i[0]].append([i[1], i[2]])
     graph[i[1]].append([i[0], i[2]])
 
-result = sys.maxsize
+result = 0
+for i in mst:
+    result += i[2]*2
 for i in range(1, n+1):
-    temp = 0
-    visited = [0]*(n+1)
-    # dfs(i)
-    cur = i
-    stack = []
-    visited[cur] = 1
-    while True:
-        temp += cost[cur]
-        check = True
-        for j in graph[cur]:
-            if visited[j[0]] == 0:
-                visited[j[0]] = 1
-                temp += j[1]
-                stack.append([cur, j[1]])
-                cur = j[0]
-                check = False
-                break
-        if check:
-            if stack:
-                cur, v = stack.pop()
-                temp += v
-            else:
-                break
-    if temp < result:
-        result = temp
-
+    if graph[i]:
+        result += cost[i]*len(graph[i])
+result += min(cost)
 print(result)
+
+
