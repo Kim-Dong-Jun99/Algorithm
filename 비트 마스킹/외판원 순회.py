53f4d702fbd597e37019496e9d629116b1cdd708
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-22 16:45:51 +0900

    다시 풀어도 시간 초과인 외판원 순회 문제

--- 비트 마스킹/외판원 순회.py
+++ 비트 마스킹/외판원 순회.py
@@ -1,28 +1,26 @@
 import sys
 n = int(sys.stdin.readline())
-cost = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+dp = [[0 for _ in range(n)] for _ in range(2**n+1)]
+# dp[1].append(0)
 
-start = 0
-cur = 0
-visited = 0b1
-endcheck = 0b0
-for i in range(n):
-    endcheck |= (1 << i)
 result = sys.maxsize
 
+def dfs(cur, visited):
+    print(dp)
+    check = True
+    for i in range(n):
+        if visited & (1 << i) != (1 << i) and graph[cur][i] != 0:
+            dp[visited | (1 << i)][i] = dp[visited][cur] + graph[cur][i]
+            nvisited = visited | (1 << i)
+            check = False
+            dfs(i, nvisited)
+    if check and visited == 2**n-1 and graph[cur][0] != 0:
+        global result
+        if result > dp[visited][cur] + graph[cur][0]:
+            result = dp[visited][cur] + graph[cur][0]
 
-def bt(cur, visited, v):
-    if endcheck & visited == endcheck:
-        if cost[cur][start] != 0:
-            temp = v + cost[cur][start]
-            global result
-            if result > temp:
-                result = temp
-    else:
-        for i in range(n):
-            if visited & (1 << i) != (1 << i) and cost[cur][i] != 0:
-                bt(i, visited | (1 << i), v + cost[cur][i])
 
-bt(cur, visited, 0)
-
+dfs(0, 1)
+# print(dp)
 print(result)
