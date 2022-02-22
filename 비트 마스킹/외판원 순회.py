d04b0d29ed2d20a36389f698aa9f16e8b49ebd3a
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-22 17:03:40 +0900

    구글링을 통해 시간 초과 해결,

--- 비트 마스킹/외판원 순회.py
+++ 비트 마스킹/외판원 순회.py
@@ -1,26 +1,32 @@
-import sys
-n = int(sys.stdin.readline())
-graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-dp = [[0 for _ in range(n)] for _ in range(2**n+1)]
-# dp[1].append(0)
+n = int(input())
 
-result = sys.maxsize
-
-def dfs(cur, visited):
-    print(dp)
-    check = True
-    for i in range(n):
-        if visited & (1 << i) != (1 << i) and graph[cur][i] != 0:
-            dp[visited | (1 << i)][i] = dp[visited][cur] + graph[cur][i]
-            nvisited = visited | (1 << i)
-            check = False
-            dfs(i, nvisited)
-    if check and visited == 2**n-1 and graph[cur][0] != 0:
-        global result
-        if result > dp[visited][cur] + graph[cur][0]:
-            result = dp[visited][cur] + graph[cur][0]
+INF = int(1e9)
+dp = [[INF] * (1 << n) for _ in range(n)]
 
 
-dfs(0, 1)
-# print(dp)
-print(result)
+def dfs(x, visited):
+    if visited == (1 << n) - 1:
+        if graph[x][0]:
+            return graph[x][0]
+        else:
+            return INF
+
+    if dp[x][visited] != INF:
+        return dp[x][visited]
+
+    for i in range(1, n):
+        if not graph[x][i]:
+            continue
+        if visited & (1 << i):
+            continue
+
+
+        dp[x][visited] = min(dp[x][visited], dfs(i, visited | (1 << i)) + graph[x][i])
+    return dp[x][visited]
+
+
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input().split())))
+
+print(dfs(0, 1))
