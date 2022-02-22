8f1ee2c09c227aeba6bdb136d5ff64081d51ec10
Kim-Dong-Jun <kimdongjun.business@gmail.com>
2022-02-22 10:49:01 +0900

    타일에서 가장 큰 정사각형 구하기, 틀린 부분 있음

---
+++ 다이나믹 프로그래밍/가장 큰 정사각형.py
@@ -0,0 +1,33 @@
+import sys
+
+n, m = map(int, sys.stdin.readline().split())
+rec = [sys.stdin.readline().strip() for _ in range(n)]
+
+
+dp = [[0 for _ in range(m)] for _ in range(n)]
+
+
+for i in range(m):
+    if rec[0][i] == '1':
+        dp[0][i] = 1
+curmax = 1
+for i in range(1, n):
+    for j in range(m):
+        if rec[i][j] == '1':
+            dp[i][j] = dp[i-1][j] + 1
+        else:
+            dp[i][j] = 0
+    con = 0
+    maxcon = 0
+    for j in range(m):
+        if dp[i][j] > curmax:
+            con += 1
+            if con > maxcon:
+                maxcon = con
+        else:
+            con = 0
+    if maxcon > curmax:
+        curmax += 1
+
+print(curmax ** 2)
+
