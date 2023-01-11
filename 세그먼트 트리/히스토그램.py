e277e3837045bde8ea04cc98e992153638f03c85
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-11 20:02:40 +0900

    다이나믹 프로그래밍 - 신나는 함수 실행

--- 세그먼트 트리/히스토그램.py
+++ 세그먼트 트리/히스토그램.py
@@ -30,32 +30,42 @@ def createSegmentTree(left, right):
 root = createSegmentTree(0, n - 1)
 
 
-def search_max_length(value, targetLeft, targetRight, left, right, node):
-    if right < targetLeft:
-        return 0
+def getMaxRect(node, left, right):
+    curMax = node.value * (right - left + 1)
     if left == right:
-        if value <= node.value:
-            return 1
-        else:
-            return 0
+        return curMax
     middle = getMiddle(left, right)
-    totalLength = 0
-    rightValue = 0
     if node.leftChild != None:
-        totalLength = search_max_length(value, targetLeft, targetRight, left, middle, node.leftChild)
-    if node.rightChild != None:
-        rightValue += search_max_length(value, targetLeft, targetRight, middle + 1, right, node.rightChild)
-    if left < targetLeft or totalLength == middle - left + 1:
-        totalLength += rightValue
-
-    return totalLength
 
 
-maxSize = n * root.value
-for i in range(n):
-    tempSize = ns[i] * search_max_length(ns[i], i, n - 1, 0, n - 1, root)
-    # print(tempSize)
-    if tempSize > maxSize:
-        maxSize = tempSize
 
-print(maxSize)
+
+# def search_max_length(value, targetLeft, targetRight, left, right, node):
+#     if right < targetLeft:
+#         return 0
+#     if left == right:
+#         if value <= node.value:
+#             return 1
+#         else:
+#             return 0
+#     middle = getMiddle(left, right)
+#     totalLength = 0
+#     rightValue = 0
+#     if node.leftChild != None:
+#         totalLength = search_max_length(value, targetLeft, targetRight, left, middle, node.leftChild)
+#     if node.rightChild != None:
+#         rightValue += search_max_length(value, targetLeft, targetRight, middle + 1, right, node.rightChild)
+#     if left < targetLeft or totalLength == middle - left + 1:
+#         totalLength += rightValue
+#
+#     return totalLength
+#
+#
+# maxSize = n * root.value
+# for i in range(n):
+#     tempSize = ns[i] * search_max_length(ns[i], i, n - 1, 0, n - 1, root)
+#     # print(tempSize)
+#     if tempSize > maxSize:
+#         maxSize = tempSize
+#
+# print(maxSize)
