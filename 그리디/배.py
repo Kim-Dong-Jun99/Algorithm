a7f93a5c07d189f3f02b35ee9635779744a299cc
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-22 19:58:10 +0900

    그리디 - 배

--- 그리디/배.py
+++ 그리디/배.py
@@ -1,5 +1,4 @@
 import sys
-
 N = int(sys.stdin.readline())
 limits = list(map(int, sys.stdin.readline().split()))
 M = int(sys.stdin.readline())
@@ -7,21 +6,36 @@ boxes = list(map(int, sys.stdin.readline().split()))
 
 limits.sort()
 boxes.sort()
+can_remove = [0] * N
 result = 0
-
 if limits[-1] < boxes[-1]:
 	print(-1)
 else:
 	limits_index = 0
-	boxes_index = 0
-	while boxes_index < M:
+	for i in range(M):
+		if limits[limits_index] >= boxes[i]:
+			can_remove[limits_index] += 1
+		else:
+			while limits[limits_index] < boxes[i]:
+				limits_index += 1
+			can_remove[limits_index] += 1
+	while True:
+		stacked = 0
+		zero_count = 0
 		result += 1
-		while limits[limits_index] < boxes[boxes_index]:
-			limits_index += 1
-		for i in range(limits_index, N):
-			if limits[i] >= boxes[boxes_index]:
-				boxes_index += 1	
-				if boxes_index == M:
-					break
-		
+		for i in range(N-1, -1, -1):
+			if can_remove[i] > 0:
+				stacked += 1
+				if can_remove[i] - stacked < 0:
+					stacked -= can_remove[i]
+					can_remove[i] = 0
+				else:
+					can_remove[i] -= stacked
+					stacked = 0
+			else:
+				stacked += 1
+			if can_remove[i] == 0:
+				zero_count += 1
+		if zero_count == N:
+			break
 	print(result)
