3be40106e9707c563b1ae1d02704aa6431c5cf7e
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-22 23:23:20 +0900

    그리디 - 주식

---
+++ 그리디/주식.py
@@ -0,0 +1,13 @@
+import sys
+T = int(sys.stdin.readline())
+for _ in range(T):
+    n = int(sys.stdin.readline())
+    nums = list(map(int, sys.stdin.readline().split()))
+    value=0
+    max=0
+    for i in range(len(nums)-1,-1,-1):
+        if(nums[i] > max):
+            max = nums[i]
+        else:
+            value+=max-nums[i]
+    print(value)
