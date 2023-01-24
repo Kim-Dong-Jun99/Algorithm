2e10e4e02b7d6089f0f900f25828422c6ea58bbf
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-24 16:58:42 +0900

    그리디 - 도서관, 테스트 케이스 전부 통과하게 수정해야함

---
+++ 그리디/도서관.py
@@ -0,0 +1,70 @@
+import sys
+
+N, M = map(int, sys.stdin.readline().split())
+
+locations = list(map(int, sys.stdin.readline().split()))
+locations.append(0)
+locations.sort()
+zero_index = locations.index(0)
+result = 0
+negative = zero_index
+positive = N - zero_index
+if locations[N] == 0:
+	repeat_time = negative // M
+	remainder = negative % M
+	if repeat_time != 0:
+		for i in range(repeat_time):
+			if i == 0:
+				result += abs(locations[i*M])
+			else:
+				result += abs(locations[i*M]*2)
+		result += abs(locations[zero_index - remainder]*2)
+	else:
+		result += abs(locations[0])
+	print(result)
+elif locations[0] == 0:
+	repeat_time = positive // M
+	remainder = positive % M
+	if repeat_time != 0:
+		for i in range(repeat_time):
+			if i == 0:
+				result += locations(N-i*M)
+			else:
+				result += locations[N - i*M]*2
+		result += locations[zero_index + remainder]*2
+	else:
+		result += locations[N]
+	print(result)
+else:
+	if abs(locations[0]) > abs(locations[N]):
+		# 음수가 더 큼
+		repeat_time = positive // M
+		remainder = positive % M
+		for i in range(repeat_time):
+			result += locations[N - i*M]*2
+		result += locations[zero_index + remainder]*2
+		repeat_time = negative // M
+		remainder = negative % M
+		for i in range(repeat_time):
+			if i == 0:
+				result += abs(locations[i*M])
+			else:
+				result += abs(locations[i*M]*2)
+		result += abs(locations[zero_index - remainder]*2)
+	else: 
+		# 양수가 더 큼
+		repeat_time = negative // M
+		remainder = negative % M
+		for i in range(repeat_time):
+			result += abs(locations[i*M]*2)
+		result += abs(locations[zero_index - remainder]*2)
+		repeat_time = positive // M
+		remainder = positive % M
+		for i in range(repeat_time):
+			if i == 0:
+				result += locations[N - i*M]
+			else:
+				result += locations[N - i*M]*2
+		result += locations[zero_index + remainder]*2
+
+	print(result)
