efd2229845a6a1f2d4e1e0c2fc271df4ac378e18
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-25 16:27:18 +0900

    그리디 - 도서관

--- 그리디/도서관.py
+++ 그리디/도서관.py
@@ -10,61 +10,78 @@ result = 0
 negative = zero_index
 positive = N - zero_index
 if locations[N] == 0:
-	repeat_time = negative // M
-	remainder = negative % M
-	if repeat_time != 0:
-		for i in range(repeat_time):
-			if i == 0:
-				result += abs(locations[i*M])
-			else:
-				result += abs(locations[i*M]*2)
-		result += abs(locations[zero_index - remainder]*2)
-	else:
-		result += abs(locations[0])
-	print(result)
+    repeat_time = negative // M
+    remainder = negative % M
+    if repeat_time != 0:
+        for i in range(repeat_time):
+            if i == 0:
+                result += abs(locations[i * M])
+            else:
+                result += abs(locations[i * M] * 2)
+        result += abs(locations[zero_index - remainder] * 2)
+    else:
+        result += abs(locations[0])
+# 	print(result)
 elif locations[0] == 0:
-	repeat_time = positive // M
-	remainder = positive % M
-	if repeat_time != 0:
-		for i in range(repeat_time):
-			if i == 0:
-				result += locations(N-i*M)
-			else:
-				result += locations[N - i*M]*2
-		result += locations[zero_index + remainder]*2
-	else:
-		result += locations[N]
-	print(result)
+    repeat_time = positive // M
+    remainder = positive % M
+    if repeat_time != 0:
+        for i in range(repeat_time):
+            if i == 0:
+                result += locations[N - i * M]
+            else:
+                result += locations[N - i * M] * 2
+        result += locations[zero_index + remainder] * 2
+    else:
+        result += locations[N]
+# 	print(result)
 else:
-	if abs(locations[0]) > abs(locations[N]):
-		# 음수가 더 큼
-		repeat_time = positive // M
-		remainder = positive % M
-		for i in range(repeat_time):
-			result += locations[N - i*M]*2
-		result += locations[zero_index + remainder]*2
-		repeat_time = negative // M
-		remainder = negative % M
-		for i in range(repeat_time):
-			if i == 0:
-				result += abs(locations[i*M])
-			else:
-				result += abs(locations[i*M]*2)
-		result += abs(locations[zero_index - remainder]*2)
-	else: 
-		# 양수가 더 큼
-		repeat_time = negative // M
-		remainder = negative % M
-		for i in range(repeat_time):
-			result += abs(locations[i*M]*2)
-		result += abs(locations[zero_index - remainder]*2)
-		repeat_time = positive // M
-		remainder = positive % M
-		for i in range(repeat_time):
-			if i == 0:
-				result += locations[N - i*M]
-			else:
-				result += locations[N - i*M]*2
-		result += locations[zero_index + remainder]*2
+    if abs(locations[0]) > abs(locations[N]):
+        # 음수가 더 큼
+        repeat_time = positive // M
+        remainder = positive % M
+        if repeat_time != 0:
+            for i in range(repeat_time):
+                result += locations[N - i * M] * 2
+            result += locations[zero_index + remainder] * 2
+        else:
+            result += locations[N] * 2
+        repeat_time = negative // M
+        remainder = negative % M
+        if repeat_time != 0:
+            for i in range(repeat_time):
+                if i == 0:
+                    result += abs(locations[i * M])
+                else:
+                    result += abs(locations[i * M] * 2)
+            result += abs(locations[zero_index - remainder] * 2)
+        else:
+            result += abs(locations[0])
 
-	print(result)
+
+    else:
+        # 양수가 더 큼
+        repeat_time = negative // M
+        remainder = negative % M
+        if repeat_time != 0:
+            for i in range(repeat_time):
+                result += abs(locations[i * M] * 2)
+            result += abs(locations[zero_index - remainder] * 2)
+        else:
+            result += abs(locations[0]) * 2
+
+
+        repeat_time = positive // M
+        remainder = positive % M
+        if repeat_time != 0:
+            for i in range(repeat_time):
+                if i == 0:
+                    result += locations[N - i * M]
+                else:
+                    result += locations[N - i * M] * 2
+            result += locations[zero_index + remainder] * 2
+        else:
+            result += locations[N]
+
+
+print(result)
