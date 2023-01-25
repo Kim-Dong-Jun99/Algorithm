471edd5e1784f3a6fbbb7231dc7d0bce4771f63f
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-25 16:52:15 +0900

    그리디 - 스네이크 버드

---
+++ 그리디/스네이크 버드.py
@@ -0,0 +1,15 @@
+import sys
+
+N, L = map(int, sys.stdin.readline().split())
+
+fruits = list(map(int, sys.stdin.readline().split()))
+
+fruits.sort()
+
+for i in fruits:
+	if i <= L:
+		L += 1
+	else:
+		break
+		
+print(L)
