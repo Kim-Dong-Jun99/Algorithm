d7d5f6baea421ad206a03afeceddb2aa551e5a55
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-28 09:33:33 +0900

    그리디 - 팔

--- 그리디/팔.py
+++ 그리디/팔.py
@@ -1,2 +1,13 @@
-L,R = map(int, input().split())
+L, R = input().split()
 
+if len(R) > len(L):
+	print(0)
+else:
+	result = 0
+	for i in range(len(R)):
+		if L[i] == R[i]:
+			if L[i] == '8':
+				result += 1
+		else:
+			break
+	print(result)
