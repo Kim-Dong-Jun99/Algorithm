d3e98a15066c090752e8774b3bfac24fcc5b5abc
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-31 00:56:57 +0900

    그리디 - 행복 유치원

--- 그리디/행복 유치원.py
+++ 그리디/행복 유치원.py
@@ -4,27 +4,19 @@ N, K = map(int, sys.stdin.readline().split())
 
 kids = list(map(int, sys.stdin.readline().split()))
 
-# (m-1)*x = N-K, x = 조원이 한명이 아닌 조의 개수, m = 한명이 아닌 조의 조원 수
+diffs = []
+for i in range(N-1):
+	diffs.append([i,i+1,kids[i+1] - kids[i]])
 
-left = 0
-right = N-1
-result = 0
-X = N - K
-ones = N - 2*X
-if ones >= 0:
-	for i in range(ones):
-		# 왼쪽 합이 더 작음
-		if kids[right-1] - kids[left] < kids[right] -  kids[left+1]:
-			right -= 1
-		else:
-			left += 1
-	for i in range(left,right,2):
-		result += kids[i+1] - kids[i]
-else:
+diffs.sort(key = lambda x:x[2], reverse = True)
 
-	
-	
+print(diffs)
+
+result = kids[-1] - kids[0]
+
+for i in range(K-1):
+	result -= diffs[i][2]
 print(result)
-
+	
 
 
