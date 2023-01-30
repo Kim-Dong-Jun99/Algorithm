4ff83d2a7e7abe4019c87f9d1d7021a4b36560e6
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-01-30 10:52:51 +0900

    그리디 - 행복 유치원, 테스트 케이스 모두 통과하게 수정해야함

--- 그리디/행복 유치원.py
+++ 그리디/행복 유치원.py
@@ -4,3 +4,27 @@ N, K = map(int, sys.stdin.readline().split())
 
 kids = list(map(int, sys.stdin.readline().split()))
 
+# (m-1)*x = N-K, x = 조원이 한명이 아닌 조의 개수, m = 한명이 아닌 조의 조원 수
+
+left = 0
+right = N-1
+result = 0
+X = N - K
+ones = N - 2*X
+if ones >= 0:
+	for i in range(ones):
+		# 왼쪽 합이 더 작음
+		if kids[right-1] - kids[left] < kids[right] -  kids[left+1]:
+			right -= 1
+		else:
+			left += 1
+	for i in range(left,right,2):
+		result += kids[i+1] - kids[i]
+else:
+
+	
+	
+print(result)
+
+
+
