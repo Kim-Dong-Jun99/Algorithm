840549b671d09a7a02cfa057e8631d9b5be04936
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-02 15:04:25 +0900

    그래프 이론 - 이분 그래프

--- 그래프 이론/이분 그래프.py
+++ 그래프 이론/이분 그래프.py
@@ -12,25 +12,29 @@ for _ in range(K):
 		i, j = map(int, sys.stdin.readline().split())
 		graph[i-1].append(j-1)
 		graph[j-1].append(i-1)
-	go = [0]
-	visited[0] = 1
-	done = False
-	while go:
-		temp = []
-		for i in go:
-			for j in graph[i]:
-				if visited[j] == 0:
-					visited[j] = visited[i] * -1
-					temp.append(j)
-				else:
-					if visited[i] * visited[j] == 1:
-						done = True
+	for i in range(V):
+		done = False
+		if visited[i] == 0:
+			go = [i]
+			visited[i] = 1
+			while go:
+				temp = []
+				for i in go:
+					for j in graph[i]:
+						if visited[j] == 0:
+							visited[j] = visited[i] * -1
+							temp.append(j)
+						else:
+							if visited[i] * visited[j] == 1:
+								done = True
+								break
+					if done:
 						break
-			if done:
-				break
+				if done:
+					break
+				go = temp
 		if done:
 			break
-		go = temp
 	if done:
 		print("NO")
 	else:
