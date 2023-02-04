316c3d3e65d5cbd8f275695068bd4e299c69b9fc
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-05 01:11:06 +0900

    그래프 이론 - 빙산..

--- 그래프 이론/빙산.py
+++ 그래프 이론/빙산.py
@@ -1,91 +1,47 @@
 import sys
-N, M = map(int, sys.stdin.readline().split())
-glacier = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+from collections import deque
 
-# 빙산 개수 카운팅, 빙산 개수 카운팅하면서, 빙산의 각 칸과 인접한 물의 수 구함
-# 빙산 개수 >= 2면 루프 끝내고 값 출력, 아니면, 계속 진행
-
-def get_neighbor(i, j):
-    temp = []
-    temp_ = 0
-    if i - 1 >= 0:
-        if glacier[i-1][j] > 0:
-            if visited[i-1][j] == 0:
-                temp.append([i-1, j])
-        else:
-            if melt[i-1][j] == 0:
-                temp_ += 1
-    if i + 1 < N:
-        if glacier[i+1][j] > 0:
-            if visited[i+1][j] == 0:
-                temp.append(([i+1, j]))
-        else:
-            if melt[i+1][j] == 0:
-                temp_ += 1
-    if j - 1 >= 0:
-        if glacier[i][j-1] > 0:
-            if visited[i][j-1] == 0:
-                temp.append([i, j-1])
-        else:
-            if melt[i][j-1] == 0:
-                temp_ += 1
-    if j + 1 < M:
-        if glacier[i][j+1] > 0:
-            if visited[i][j+1] == 0:
-                temp.append([i, j+1])
-        else:
-            if melt[i][j+1] == 0:
-                temp_ += 1
-    return [temp, temp_]
 
 def bfs(a, b):
-    go = [[a, b]]
-    result = [[a, b]]
+    go = deque([[a, b]])
+    visited[a][b] = True
     while go:
-        temp = []
-        for i, j in go:
-            neighbors, melt_ = get_neighbor(i, j)
-            for k, l in neighbors:
-                visited[k][l] = 1
-                temp.append([k, l ])
-                result.append([k, l])
-            melt[i][j] = melt_
-            if glacier[i][j] >= melt_:
-                glacier[i][j] -= melt_
-            else:
-                glacier[i][j] = 0
+        x, y = go.popleft()
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            # ices = 0
+            if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
+                if glacier[nx][ny] != 0:
+                    visited[nx][ny] = True
+                    go.append([nx, ny])
+                else:
+                    count[x][y] += 1
+    return 0
 
-        go = temp
 
-    return result
-
+N, M = map(int, sys.stdin.readline().split())
+glacier = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 result = 0
-success = False
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+
 while True:
-    visited = [[0 for _ in range(M)] for _ in range(N)]
-    melt = [[0 for _ in range(M)] for _ in range(N)]
-    to_melt = []
+    visited = [[False] * M for _ in range(N)]
+    count = [[0] * M for _ in range(N)]
+    answer = []
     for i in range(N):
         for j in range(M):
-            if glacier[i][j] > 0 and visited[i][j] == 0:
-                visited[i][j] = 1
-                to_melt.append(bfs(i, j))
+            if glacier[i][j] != 0 and visited[i][j] == 0:
+                answer.append(bfs(i, j))
 
-    if len(to_melt) >= 2:
-        success = True
+    for i in range(N):
+        for j in range(M):
+            glacier[i][j] -= count[i][j]
+            if glacier[i][j] < 0:
+                glacier[i][j] = 0
+    if len(answer) == 0 or len(answer) >= 2:
         break
+
     result += 1
-
-    # for ice in to_melt:
-    #     for i, j in ice:
-    #         if glacier[i][j] >= melt[i][j]:
-    #             glacier[i][j] -= melt[i][j]
-    #         else:
-    #             glacier[i][j] = 0
-
-
-if success:
-    print(result)
-else:
-    print(0)
-
+print(result) if len(answer) >= 2 else print(0)
