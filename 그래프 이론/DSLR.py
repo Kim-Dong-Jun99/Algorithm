d81d4331a180fc54aa6857659d03674717fb4ab0
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-05 15:32:47 +0900

    그래프 이론 - DSLR

--- 그래프 이론/DSLR.py
+++ 그래프 이론/DSLR.py
@@ -1,62 +1,39 @@
 import sys
-from collections import defaultdict
+from collections import defaultdict, deque
 
-def match_len(a):
-	if len(a) < 4:
-		a = '0' * (4 - len(a)) + a
-	return a
-
-def match_len_int(a):
-	a = str(a)
-	return match_len(a)
-# a = str, a_ = int
 def can_go(a):
-	temp = []
-	a_ = int(a)
-	
-	D = (a_ * 2) % 10000
-	D = match_len_int(D)
-	temp.append([D, 'D'])
-	
-	L = a_ - 1
-	if L == -1:
-		L = 9999
-	L = match_len_int(L)
-	temp.append([L, 'L'])
-	
-	S = a[1:] + a[0]
+	temp = [[(a*2)%10000,'D']]
+	S = a - 1
+	if a == -1:
+		S = 9999
 	temp.append([S, 'S'])
-	
-	R = a[3] + a[:3]
+	L = (a % 1000) * 10 + (a // 1000)
+	temp.append([L, 'L'])
+	R = (a % 10)*1000 + (a // 10)
 	temp.append([R, 'R'])
 	return temp
-
-
+	
+	
 T = int(sys.stdin.readline())
-
 for _ in range(T):
-	A, B = sys.stdin.readline().split()
-	A = match_len(A)
-	B = match_len(B)
-	go = [A]
-	visited = defaultdict(int)
-	visited[A] = []
+	A, B = map(int, sys.stdin.readline().split())
+	go = deque([A])
+	visited = defaultdict(list)
+	visited[A] = [A, '']
 	while go:
-		temp = []
-		for i in go:
-			for j, l in can_go(i):
-				if visited[j] == 0:
-					temp.append(j)
-					visited[j] = [i, l]
-		go = temp
-		if visited[B] != 0:
+		X = go.popleft()
+		for j, k in can_go(X):
+			if len(visited[j]) == 0:
+				visited[j] = [X, k]
+				go.append(j)
+		if len(visited[B]) != 0:
 			parent = B
 			result = []
 			while parent != A:
 				result.append(visited[parent][1])
 				parent = visited[parent][0]
 			for i in range(len(result)-1, -1, -1):
-				print(result[i], end='')
-			print()
+				sys.stdout.write(result[i])
+			if _ != T-1:
+				sys.stdout.write('\n')
 			break
-				
