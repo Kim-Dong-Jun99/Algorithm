a82ca57b4e2fe1282bf654a0ec36ed1281ee4ba8
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-17 22:15:11 +0900

    느리게 갱신하는 세그먼트 트리 - XOR, operator 모듈 사용, 여전히 시간초과

--- 세그먼트 트리/XOR.py
+++ 세그먼트 트리/XOR.py
@@ -1,14 +1,23 @@
 import sys
-import time
 import math
+import operator
 
 sys.setrecursionlimit(1000000)
 
-# start = time.time()
 
+def smaller(i, j):
+    if operator.lt(i, j):
+        return i
+    return j
+
+
+def bigger(i, j):
+    if operator.gt(i, j):
+        return j
+    return i
 
 def get_middle(left, right):
-    return (left + right) // 2
+    return operator.floordiv(operator.add(left, right), 2)
 
 
 def create_segment_tree(index, left, right):
@@ -16,9 +25,11 @@ def create_segment_tree(index, left, right):
         tree[index] = numbers[left]
     else:
         middle = get_middle(left, right)
-        create_segment_tree(index * 2 + 1, left, middle)
-        create_segment_tree(index * 2 + 2, middle + 1, right)
-        tree[index] = tree[index * 2 + 1] ^ tree[index * 2 + 2]
+        left_c = operator.add(operator.mul(index, 2), 1)
+        right_c = operator.add(operator.mul(index, 2), 2)
+        create_segment_tree(left_c, left, middle)
+        create_segment_tree(right_c, middle + 1, right)
+        tree[index] = operator.xor(tree[left_c], tree[right_c])
 
 
 def query(index, left, right, left_limit, right_limit):
@@ -27,30 +38,35 @@ def query(index, left, right, left_limit, right_limit):
     if right_limit < left:
         return -1
     middle = get_middle(left, right)
+    left_c = operator.add(operator.mul(index, 2), 1)
+    right_c = operator.add(operator.mul(index, 2), 2)
     if lazy[index] != -1:
-        if (right - left + 1) % 2 == 1:
-            tree[index] ^= lazy[index]
-        if index * 2 + 1 < node_num:
-            if lazy[index * 2 + 1] == -1:
-                lazy[index * 2 + 1] = lazy[index]
+        sub = operator.sub(right, left)
+        add = operator.add(sub, 1)
+        mod = operator.mod(add, 2)
+        if mod == 1:
+            tree[index] = operator.xor(tree[index], lazy[index])
+        if left_c < node_num:
+            if lazy[left_c] == -1:
+                lazy[left_c] = lazy[index]
             else:
-                lazy[index * 2 + 1] ^= lazy[index]
-        if index * 2 + 2 < node_num:
-            if lazy[index * 2 + 2] == -1:
-                lazy[index * 2 + 2] = lazy[index]
+                lazy[left_c] = operator.xor(lazy[left_c], lazy[index])
+        if right_c < node_num:
+            if lazy[right_c] == -1:
+                lazy[right_c] = lazy[index]
             else:
-                lazy[index * 2 + 2] ^= lazy[index]
+                lazy[right_c] = operator.xor(lazy[right_c], lazy[index])
     lazy[index] = -1
-    if left_limit <= left and right <= right_limit:
+    if operator.le(left_limit, left) and operator.le(right, right_limit):
         return tree[index]
-    lr = query(index * 2 + 1, left, middle, left_limit, right_limit)
-    rr = query(index * 2 + 2, middle + 1, right, left_limit, right_limit)
+    lr = query(left_c, left, middle, left_limit, right_limit)
+    rr = query(right_c, middle + 1, right, left_limit, right_limit)
     if lr == -1:
         return rr
     elif rr == -1:
         return lr
 
-    return lr ^ rr
+    return operator.xor(lr, rr)
 
 
 def update(index, left, right, left_limit, right_limit, k):
@@ -60,17 +76,22 @@ def update(index, left, right, left_limit, right_limit, k):
         return
 
     middle = get_middle(left, right)
-    if left_limit <= left and right <= right_limit:
+    if operator.le(left_limit, left) and operator.le(right, right_limit):
         if lazy[index] == -1:
             lazy[index] = k
         else:
             lazy[index] ^= k
         return
+    left_c = operator.add(operator.mul(index, 2), 1)
+    right_c = operator.add(operator.mul(index, 2), 2)
+    sub = operator.sub(smaller(right, right_limit), bigger(left, left_limit))
+    add = operator.add(sub, 1)
+    mod = operator.mod(add, 2)
 
-    if (min(right, right_limit) - max(left, left_limit) + 1) % 2 == 1:
-        tree[index] ^= k
-    update(index * 2 + 1, left, middle, left_limit, right_limit, k)
-    update(index * 2 + 2, middle + 1, right, left_limit, right_limit, k)
+    if mod == 1:
+        tree[index] = operator.xor(tree[index], k)
+    update(left_c, left, middle, left_limit, right_limit, k)
+    update(right_c, operator.add(middle, 1), right, left_limit, right_limit, k)
 
 
 N = int(sys.stdin.readline())
@@ -86,9 +107,6 @@ numbers = list(map(int, sys.stdin.readline().split()))
 tree = [0] * node_num
 lazy = [-1] * node_num
 create_segment_tree(0, 0, N - 1)
-# tree_create_time = time.time()
-# print("tree time : %.5f" % (tree_create_time - start))
-
 M = int(sys.stdin.readline())
 
 for _ in range(M):
@@ -98,6 +116,3 @@ for _ in range(M):
         update(0, 0, N - 1, q[1], q[2], q[3])
     else:
         sys.stdout.write(str(query(0, 0, N - 1, q[1], q[2])) + "\n")
-
-# end = time.time()
-# print(f"{end - start:.5f} sec")
