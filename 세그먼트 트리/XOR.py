1df03a4676d001671f8be07186ccbf4766221f4d
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-17 21:52:33 +0900

    느리게 갱신하는 세그먼트 트리 - XOR, 세그먼트 트리 생성 방식 변경 풀이..

--- 세그먼트 트리/XOR.py
+++ 세그먼트 트리/XOR.py
@@ -1,49 +1,50 @@
 import sys
+import time
+import math
 
 sys.setrecursionlimit(1000000)
 
-
-class Node:
-
-    def __init__(self, left_child, right_child, value):
-        self.leftChild = left_child
-        self.rightChild = right_child
-        self.value = value
-        self.toChange = -1
+# start = time.time()
 
 
 def get_middle(left, right):
     return (left + right) // 2
 
 
-def create_segment_tree(left, right):
+def create_segment_tree(index, left, right):
     if left == right:
-        return Node(None, None, numbers[left])
+        tree[index] = numbers[left]
     else:
         middle = get_middle(left, right)
-        left_node = create_segment_tree(left, middle)
-        right_node = create_segment_tree(middle + 1, right)
-        return Node(left_node, right_node, left_node.value ^ right_node.value)
+        create_segment_tree(index * 2 + 1, left, middle)
+        create_segment_tree(index * 2 + 2, middle + 1, right)
+        tree[index] = tree[index * 2 + 1] ^ tree[index * 2 + 2]
 
 
-def query(node, left, right, left_limit, right_limit):
+def query(index, left, right, left_limit, right_limit):
     if right < left_limit:
         return -1
     if right_limit < left:
         return -1
     middle = get_middle(left, right)
-    if (right - left + 1) % 2 == 1 and node.toChange != -1:
-        node.value ^= node.toChange
-        if node.leftChild:
-            node.leftChild.toChange ^= node.toChange
-        if node.rightChild:
-            node.rightChild.toChange ^= node.toChange
-    node.toChange = -1
+    if lazy[index] != -1:
+        if (right - left + 1) % 2 == 1:
+            tree[index] ^= lazy[index]
+        if index * 2 + 1 < node_num:
+            if lazy[index * 2 + 1] == -1:
+                lazy[index * 2 + 1] = lazy[index]
+            else:
+                lazy[index * 2 + 1] ^= lazy[index]
+        if index * 2 + 2 < node_num:
+            if lazy[index * 2 + 2] == -1:
+                lazy[index * 2 + 2] = lazy[index]
+            else:
+                lazy[index * 2 + 2] ^= lazy[index]
+    lazy[index] = -1
     if left_limit <= left and right <= right_limit:
-        return node.value
-
-    lr = query(node.leftChild, left, middle, left_limit, right_limit)
-    rr = query(node.rightChild, middle + 1, right, left_limit, right_limit)
+        return tree[index]
+    lr = query(index * 2 + 1, left, middle, left_limit, right_limit)
+    rr = query(index * 2 + 2, middle + 1, right, left_limit, right_limit)
     if lr == -1:
         return rr
     elif rr == -1:
@@ -52,7 +53,7 @@ def query(node, left, right, left_limit, right_limit):
     return lr ^ rr
 
 
-def update(node, left, right, left_limit, right_limit, k):
+def update(index, left, right, left_limit, right_limit, k):
     if right < left_limit:
         return
     if right_limit < left:
@@ -60,22 +61,33 @@ def update(node, left, right, left_limit, right_limit, k):
 
     middle = get_middle(left, right)
     if left_limit <= left and right <= right_limit:
-        if node.toChange == -1:
-            node.toChange = k
+        if lazy[index] == -1:
+            lazy[index] = k
         else:
-            node.toChange ^= k
+            lazy[index] ^= k
         return
 
     if (min(right, right_limit) - max(left, left_limit) + 1) % 2 == 1:
-        node.value ^= k
-    update(node.leftChild, left, middle, left_limit, right_limit, k)
-    update(node.rightChild, middle + 1, right, left_limit, right_limit, k)
+        tree[index] ^= k
+    update(index * 2 + 1, left, middle, left_limit, right_limit, k)
+    update(index * 2 + 2, middle + 1, right, left_limit, right_limit, k)
 
 
 N = int(sys.stdin.readline())
 
+H = math.log2(N)
+
+if int(H) == math.ceil(H):
+    node_num = 2 * N - 1
+else:
+    node_num = 2 ** (math.ceil(H) + 1) - 1
+
 numbers = list(map(int, sys.stdin.readline().split()))
-root = create_segment_tree(0, N - 1)
+tree = [0] * node_num
+lazy = [-1] * node_num
+create_segment_tree(0, 0, N - 1)
+# tree_create_time = time.time()
+# print("tree time : %.5f" % (tree_create_time - start))
 
 M = int(sys.stdin.readline())
 
@@ -83,6 +95,9 @@ for _ in range(M):
     q = list(map(int, sys.stdin.readline().split()))
 
     if q[0] == 1:
-        update(root, 0, N - 1, q[1], q[2], q[3])
+        update(0, 0, N - 1, q[1], q[2], q[3])
     else:
-        sys.stdout.write(str(query(root, 0, N - 1, q[1], q[2])) + "\n")
+        sys.stdout.write(str(query(0, 0, N - 1, q[1], q[2])) + "\n")
+
+# end = time.time()
+# print(f"{end - start:.5f} sec")
