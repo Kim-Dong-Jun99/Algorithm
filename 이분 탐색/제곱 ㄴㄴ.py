3ae13598d719f9269d8196f529e2c2f57fc1565e
Kim-Dong-Jun99 <kimdongjun.business@gmail.com>
2023-02-19 00:46:54 +0900

    이분 탐색 - 제곱 ㄴㄴ

--- 이분 탐색/제곱 ㄴㄴ.py
+++ 이분 탐색/제곱 ㄴㄴ.py
@@ -14,29 +14,25 @@ def setup():
 def sq_no_no_count(n):
     count = 0
     for i in range(1, int(math.sqrt(n)) + 1):
-        count += mul[i] * (n / (i * i))
+        count += mul[i] * (n // (i * i))
     return count
 
 
 K = int(sys.stdin.readline())
 
 setup()
-print("setup done")
+# print("setup done")
 left = 1
 right = 10 ** 11
-index = 0
-while index < 50:
+while True:
     middle = (left + right) // 2
 
     temp_v = sq_no_no_count(middle)
-    print("middle : temp_v = %d %d" % (middle, temp_v))
-    if temp_v == K:
+    if temp_v == K and left == right:
         print(middle)
         sys.exit()
 
-    if temp_v > K:
+    if temp_v >= K:
         right = middle
     else:
         left = middle + 1
-    index += 1
-print(sq_no_no_count(19))
