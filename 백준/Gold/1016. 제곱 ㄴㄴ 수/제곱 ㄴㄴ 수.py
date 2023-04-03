6fba71ee301510ab4a6e94b476ccc092c3201498
KIM DONGJUN <djkim35417@gmail.com>
2023-04-03 13:20:46 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 156 ms, Memory: 133152 KB -BaekjoonHub

---
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -0,0 +1,23 @@
+import sys
+left,right = map(int,sys.stdin.readline().split())
+nopower = [0 for _ in range(right-left+1)]
+npnum = right-left+1
+
+for i in range(2,int(right**0.5)+1):
+    # check = True
+    # for j in range(2,int(i**0.5)+1):
+        # if i%j == 0:
+        #     check = False
+        #     break
+
+    temp = i**2
+    s = left // temp
+    temp = temp * s
+    while temp <= right:
+        if temp >= left:
+            if nopower[temp-left] == 0:
+                npnum -= 1
+            nopower[temp-left] = 1
+        temp += i**2
+
+print(npnum)
