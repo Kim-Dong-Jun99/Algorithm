b9b04c895f630b8f1c4451af4d76c632b2800cae
KIM DONGJUN <djkim35417@gmail.com>
2023-04-08 19:39:11 +0900

    [unrated] Title: 대충 만든 자판, Time: 0.46 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -0,0 +1,27 @@
+def solution(keymap, targets):
+    """
+    19:32
+    """
+    answer = []
+    keyTable = {}
+    for i in keymap:
+        index = 0
+        for j in i:
+            index += 1
+            if j not in keyTable:
+                keyTable[j] = index
+            else:
+                if keyTable[j] > index:
+                    keyTable[j] = index
+    for i in targets:
+        tempSum = 0
+        for j in i:
+            if j in keyTable:
+                tempSum += keyTable[j]
+            else:
+                tempSum = -1
+                break
+        answer.append(tempSum)
+            
+    
+    return answer
