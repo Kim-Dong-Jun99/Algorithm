64a5c856707675d8d75dddb752d2da0cd6857438
KIM DONGJUN <djkim35417@gmail.com>
2023-04-22 13:50:03 +0900

    [unrated] Title: 테이블 해시 함수, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/147354. 테이블 해시 함수/테이블 해시 함수.py
@@ -0,0 +1,10 @@
+def solution(data, col, row_begin, row_end):
+    answer = 0
+    data.sort(key=lambda x : x[col-1]*1000001 - x[0])
+    # print(data)
+    for i in range(row_begin-1, row_end):
+        temp = 0
+        for j in data[i]:
+            temp += j % (i+1)
+        answer ^= temp
+    return answer
