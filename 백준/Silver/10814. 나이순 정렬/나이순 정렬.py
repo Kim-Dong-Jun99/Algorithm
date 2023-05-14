e0467dbb895d3ec9da868994468bd7632bec421e
KIM DONGJUN <djkim35417@gmail.com>
2023-05-14 12:06:10 +0900

    [Silver V] Title: 나이순 정렬, Time: 284 ms, Memory: 52120 KB -BaekjoonHub

---
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.py
@@ -0,0 +1,17 @@
+import sys
+n = int(sys.stdin.readline())
+table = {}
+query = [list(sys.stdin.readline().split()) for i in range(n)]
+for i in query:
+    try:
+        temp = table[int(i[0])]
+        temp.append(i[1])
+        table[int(i[0])] = temp
+    except:
+        table[int(i[0])] = [i[1]]
+        
+age = list(table.keys())
+age.sort()
+for i in age:
+    for j in table[i]:
+        print('%d %s'%(i,j))
