7cb6a23070ba9fdf9504a7f4b126ec20e6959d43
KIM DONGJUN <djkim35417@gmail.com>
2023-06-26 14:20:26 +0900

    [Silver I] Title: 물병, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1052. 물병/물병.py
@@ -0,0 +1,11 @@
+import sys
+N, K = map(int, sys.stdin.readline().split())
+
+cnt = 0
+
+while bin(N).count('1') > K:
+    idx = bin(N)[::-1].index('1')
+    cnt += 2**idx
+    N += 2**idx
+
+print(cnt)
