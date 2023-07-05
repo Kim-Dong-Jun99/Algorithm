44ab79597970682ec894eb934c6f98d130ea8676
KIM DONGJUN <djkim35417@gmail.com>
2023-07-05 18:42:31 +0900

    [unrated] Title: 표현 가능한 이진트리, Time: 37.23 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
+++ 프로그래머스/unrated/150367. 표현 가능한 이진트리/표현 가능한 이진트리.py
@@ -1,24 +1,12 @@
 import math
 def solution(numbers):
-    """
-    16:09
-    트리 탐색 순서 - LNR
-    7 - 111
-    42 - 0101010  
-    트리의 높이 h 일때, 전체 노드의 수 = 2**(h+1)-1, (h는 0부터 시작)
-    0 1 2 3 4 5 6 7 8 9 10 11 12 13 14
-    """
     answer = []
     for i in numbers:
-        # 이진수는 2번 인덱스부터 시작
         binary = str(bin(i))[2:]
         tree_height = int(math.log2(len(binary)))
         standard = '0'*(2**(tree_height+1) - 1 - len(binary)) + binary
         index = 0
         roots = [[get_middle(0, len(standard)-1), 0, len(standard)-1]]
-        # if standard[roots[0][0]] == '0' and len(standard) != 1:
-        #     answer.append(0)
-        #     continue
         possible = True
         while index < tree_height:
             temp = []
@@ -41,11 +29,6 @@ def solution(numbers):
         else:
             answer.append(0)
                     
-                
-            
-        
-        
-        
     return answer
 def get_middle(i, j):
     return (i + j) // 2
