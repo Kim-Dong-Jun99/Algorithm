c235587cc6b37dd24de6a6a281337f94a1056a42
KIM DONGJUN <djkim35417@gmail.com>
2023-07-07 19:00:52 +0900

    [unrated] Title: 아방가르드 타일링, Time: 3246.40 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
+++ 프로그래머스/unrated/181186. 아방가르드 타일링/아방가르드 타일링.py
@@ -1,19 +1,19 @@
 def solution(n):
-    memo = [1, 1, 3, 10, 23, 62]
-    if n <= 5:  return memo[n]
+    dp = [1, 1, 3, 10, 23, 62]
+    if n <= 5:  return dp[n]
     four = [2, 2, 0]
     five = [2, 0, 0]
     six = [0, 0, 0]
     for i in range(6, n+1):
-        cur = memo[-1] + 2*memo[-2] + 5*memo[-3]
+        cur = dp[-1] + 2*dp[-2] + 5*dp[-3]
         idx = (i-2)%3
-        five[idx] += memo[1] << 1
+        five[idx] += dp[1] << 1
         cur += five[idx]
         idx = (i-1)%3
-        four[idx] += memo[2] << 1
+        four[idx] += dp[2] << 1
         cur += four[idx]
         idx = i%3
-        six[idx] += memo[0] << 2
+        six[idx] += dp[0] << 2
         cur += six[idx]
-        memo = memo[1:] + [cur]
-    return memo[-1] % 1_000_000_007
+        dp = dp[1:] + [cur]
+    return dp[-1] % 1_000_000_007
