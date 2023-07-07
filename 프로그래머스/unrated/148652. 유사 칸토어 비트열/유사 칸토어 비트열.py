9d7cd7ae4d894515658a4bcf29acbaa5d6897087
KIM DONGJUN <djkim35417@gmail.com>
2023-07-07 19:08:58 +0900

    [unrated] Title: 유사 칸토어 비트열, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/148652. 유사 칸토어 비트열/유사 칸토어 비트열.py
@@ -0,0 +1,16 @@
+def recursion(n,pos):
+    if n == 1:
+        return "11011"[:pos].count("1")
+    a, b = divmod(pos, 5 ** (n-1))
+    cnt = 0
+    if a <= 1:
+        cnt = 4**(n-1) * a + recursion(n-1,b)        
+    if a == 2:
+        cnt = 2 * 4**(n-1)
+    if a > 2:
+        cnt = 4**(n-1) * (a-1) + recursion(n-1,b)
+    return cnt
+    
+def solution(n, l, r):
+    answer = recursion(n,r) - recursion(n,l-1)
+    return answer
