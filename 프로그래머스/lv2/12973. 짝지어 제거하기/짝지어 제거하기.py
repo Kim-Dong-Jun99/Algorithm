69374657cb06385ac0a4b100f7afd89907166513
KIM DONGJUN <djkim35417@gmail.com>
2023-07-12 11:06:05 +0900

    [level 2] Title: 짝지어 제거하기, Time: 135.76 ms, Memory: 13.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -0,0 +1,14 @@
+from collections import deque
+def solution(s):
+    stack = deque()
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+    if len(stack) == 0:
+        return 1
+    return 0
