108224660bbca9ddffb90788fe805e8f2c3e8be8
KIM DONGJUN <djkim35417@gmail.com>
2023-07-18 11:52:30 +0900

    [Gold IV] Title: 뱀, Time: 152 ms, Memory: 14844 KB -BaekjoonHub

--- 백준/Gold/3190. 뱀/뱀.java
+++ 백준/Gold/3190. 뱀/뱀.java
@@ -1,90 +1,131 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-public class Main {
+class Main {
+    static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    static int N,K,L;
-    static int[][] board;
-    static int[] dix = {0, 1, 0, -1}; // 오아왼위
-    static int[] diy = {1, 0, -1, 0};
-    static int[][] timeDir;
-    static int[] snail_x = new int[10101];
-    static int[] snail_y = new int[10101];
+    int N;
+    int K;
+    int L;
+    // 보드
+    int[][] board;
+    // 현재 뱀의 위치를 저장하기 위한 배열
+    int[][] visited;
+    Queue<int[]> snake = new ArrayDeque<>();
+    int[] direction;
+    int[] head;
+    int length = 1;
+    int time = 0;
+    Map<Integer, String> timeToTurn;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine()); // 보드 크기
-        K = Integer.parseInt(br.readLine()); // 사과의 갯수
-        board = new int[N][N];
-        for (int i = 0; i < K; i++) {
-            st = new StringTokenizer(br.readLine());
-            int x = Integer.parseInt(st.nextToken());
-            int y = Integer.parseInt(st.nextToken());
-            board[x-1][y-1] = -2; // 사과 표시
-        }
-        L = Integer.parseInt(br.readLine()); // 뱀 방향 횟수
-        timeDir = new int[L+1][2]; // 시간에 따른 방향 변화 기록할 배열 (바뀌는 시간, 회전 방향)
-        for (int i = 0; i < L; i++) {
-            st = new StringTokenizer(br.readLine());
-            int t = Integer.parseInt(st.nextToken());
-            char tmp = st.nextToken().charAt(0);
-            int d;
-            if (tmp == 'D') {
-                d = 1; // 오른쪽 90도 회전
-            } else {
-                d = -1; // 왼쪽 90도 회전
-            }
-
-            timeDir[i][0]= t;
-            timeDir[i][1] = d;
-        }
-
-        int headIdx = 0; // head 포인터
-        int tailIdx = 0; // tail 포인터
-        int dir = 0; // 처음 방향: 오른쪽
-        int dirIdx = 0;
-        // 뱀 위치 기록
-        snail_x[headIdx] = 0;
-        snail_y[headIdx] = 0;
-        board[0][0] = -1; // 뱀 초기 위치 보드에 기록
-
-        int time = 0;
-        while(true) {
-            time++;
-            int nx = snail_x[headIdx] + dix[dir];
-            int ny = snail_y[headIdx] + diy[dir];
-
-            // 이동하려는 위치 범위 체크, 자기 자신과 닿는지 체크
-            if (nx < 0 || nx >= N || ny < 0 || ny >= N || board[nx][ny] == -1) break;
-
-            // 이동하려는 위치에 사과가 없으면 꼬리 이동
-            if (board[nx][ny] != -2) {
-                board[snail_x[tailIdx]][snail_y[tailIdx]] = 0;
-                tailIdx++;
-            } else {
-                board[nx][ny] = 0;
-            }
-
-            // 머리 이동
-            headIdx++;
-            snail_x[headIdx] = nx;
-            snail_y[headIdx] = ny;
-            board[nx][ny] = -1;
-
-
-            if (dirIdx < L && time == timeDir[dirIdx][0]) {
-                dir += timeDir[dirIdx][1];
-                dir = (dir + 4) % 4; // 0~3 인덱스로 조정
-                dirIdx++;
-            }
-
-        }
-        System.out.println(time);
-
-
+    public static void main(String[] args) throws Exception {
+        new Main().solution();
     }
 
+    void solution() throws Exception {
+        N = Integer.parseInt(BR.readLine());
+        board = new int[N][N];
+        visited = new int[N][N];
+        Arrays.stream(board).forEach(intArray -> {
+            Arrays.fill(intArray,0);
+        });
+        Arrays.stream(visited).forEach(intArray -> {
+            Arrays.fill(intArray, 0);
+        });
+
+        K = Integer.parseInt(BR.readLine());
+        for (int i = 0; i < K; i++) {
+            int[] apples = Arrays.stream(BR.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+            board[apples[0]-1][apples[1]-1] = 1;
+        }
+
+        L = Integer.parseInt(BR.readLine());
+        timeToTurn = new HashMap<>();
+        for (int i = 0; i < L; i++) {
+            String[] turnOrders = BR.readLine().split(" ");
+            timeToTurn.put(Integer.parseInt(turnOrders[0]), turnOrders[1]);
+        }
+
+        visited[0][0] = 1;
+        head = new int[]{0, 0};
+        direction = new int[]{0, 1};
+        snake.add(new int[]{0, 0});
+        while (true) {
+            head = move(head, direction);
+            time += 1;
+//            System.out.println("head = "+head[0]+", "+head[1]+" time = "+time+" length = "+length);
+            if (head[0] < 0 || head[0] >= N || head[1] < 0 || head[1] >= N) {
+                break;
+            }
+            if (visited[head[0]][head[1]] == 1) {
+                break;
+            }
+
+
+            addToSnake(head);
+
+            direction = rotate(direction, time);
+
+        }
+//        BW.write(time);
+//        BW.flush();
+//        BW.close();
+        System.out.println(time);
+    }
+
+    void addToSnake(int[] head) {
+        visited[head[0]][head[1]] = 1;
+        snake.add(head);
+        if (board[head[0]][head[1]] == 1) {
+            length += 1;
+            board[head[0]][head[1]] = 0;
+        }
+        if (snake.size() > length) {
+            int[] removed = snake.remove();
+            visited[removed[0]][removed[1]] = 0;
+        }
+    }
+
+    int[] move(int[] head, int[] direction) {
+        return new int[]{head[0] + direction[0], head[1] + direction[1]};
+    }
+
+    int[] rotate(int[] direction, int time) {
+        if (timeToTurn.containsKey(time)) {
+            String leftOrRight = timeToTurn.get(time);
+            if (Objects.equals(leftOrRight, "L")) {
+                if (direction[0] == 0 && direction[1] == 1) {
+                    // right to up
+                    return new int[]{-1, 0};
+                } else if (direction[0] == 1 && direction[1] == 0) {
+                    // down to right
+                    return new int[]{0, 1};
+                } else if (direction[0] == -1 && direction[1] == 0) {
+                    // up to left
+                    return new int[]{0, -1};
+                } else {
+                    // left to down
+                    return new int[]{1, 0};
+                }
+            } else {
+                if (direction[0] == 0 && direction[1] == 1) {
+                    // right to down
+                    return new int[]{1, 0};
+                } else if (direction[0] == 1 && direction[1] == 0) {
+                    // down to left
+                    return new int[]{0, -1};
+                } else if (direction[0] == -1 && direction[1] == 0) {
+                    // up to right
+                    return new int[]{0, 1};
+                } else {
+                    // left to up
+                    return new int[]{-1, 0};
+                }
+            }
+        } else {
+            return direction;
+        }
+    }
 }
