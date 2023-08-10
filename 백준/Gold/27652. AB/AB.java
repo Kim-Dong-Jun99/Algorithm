01ae6fd5737b04286eb58e9304e9dbcdaf9b38e7
KIM DONGJUN <djkim35417@gmail.com>
2023-08-10 11:24:03 +0900

    [Gold I] Title: AB, Time: 768 ms, Memory: 206044 KB -BaekjoonHub

--- 백준/Gold/27652. AB/AB.java
+++ 백준/Gold/27652. AB/AB.java
@@ -1,96 +1,147 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 class Main {
-
-    public static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+    static final BufferedWriter BW = new BufferedWriter(new OutputStreamWriter(System.out));
     int Q;
-    Hashtable<String, Long> A;
-    Hashtable<String, Long> B;
-    String[] commands;
-
-    public static void main(String[] args) throws Exception {
+    Trie A;
+    Trie B;
+    String[] query;
+    HashMap<String, Trie> trieMap;
+    public static void main(String[] args) {
         Main main = new Main();
-        main.init();
-        main.solution();
+        try {
+            main.init();
+            main.solution();
+        } catch(IOException e) {
+            System.out.println("Exception during I/O");
+        }
+
     }
 
-    void init() throws Exception {
+    void init() throws IOException {
         Q = Integer.parseInt(BR.readLine());
-        A = new Hashtable<>();
-        B = new Hashtable<>();
-        commands = new String[Q];
-        int i = 0;
-        while (i < Q) {
-            commands[i] = BR.readLine();
-            i += 1;
-        }
+        A = new Trie();
+        B = new Trie();
+        trieMap = new HashMap<>();
+        trieMap.put("A", A);
+        trieMap.put("B", B);
     }
-
-    void find(String toFind) {
-        long answer = 0;
-        StringBuilder findA = new StringBuilder();
-        StringBuilder findB = new StringBuilder(toFind);
-        for (int i = 0; i < toFind.length(); i++) {
-            findA.append(toFind.charAt(i));
-            findB.deleteCharAt(0);
-            String strA = findA.toString();
-            String strB = findB.toString();
-            if (A.containsKey(strA) && B.containsKey(strB)) {
-                answer += A.get(strA) * B.get(strB);
-            }
-        }
-        System.out.println(answer);
-    }
-    void add(String AB, String toAdd) {
-        if (AB.equals("A")) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < toAdd.length(); i++) {
-                sb.append(toAdd.charAt(i));
-                String str = sb.toString();
-                A.put(str, (A.containsKey(str) ? A.get(str) + 1 : 1));
-            }
-        } else {
-            StringBuilder sb = new StringBuilder(toAdd);
-            for (int i = 0; i < toAdd.length(); i++) {
-                String str = sb.toString();
-                B.put(str, (B.containsKey(str) ? B.get(str) + 1 : 1));
-                sb.deleteCharAt(0);
-            }
-        }
-    }
-
-    void delete(String AB, String toDelete) {
-        if (AB.equals("A")) {
-            StringBuilder sb = new StringBuilder();
-            for (int i = 0; i < toDelete.length(); i++) {
-                sb.append(toDelete.charAt(i));
-                String str = sb.toString();
-                A.put(str, (A.containsKey(str) ? A.get(str) - 1 : 0));
-            }
-        } else {
-            StringBuilder sb = new StringBuilder(toDelete);
-            for (int i = 0; i < toDelete.length(); i++) {
-                String str = sb.toString();
-                B.put(str, (B.containsKey(str) ? B.get(str) - 1 : 0));
-                sb.deleteCharAt(0);
-            }
-        }
-    }
-    
-    void solution() {
-        for (String command : commands) {
-            String[] query = command.split(" ");
+    void solution() throws IOException {
+        while (Q-- > 0) {
+            query = BR.readLine().split(" ");
             if (query[0].equals("find")) {
-                find(query[1]);
+                findString(query[1]);
             } else if (query[0].equals("add")) {
-                add(query[1], query[2]);
+                addToTrie(query[1], query[2]);
             } else {
-                delete(query[1], query[2]);
+                deleteString(query[1], query[2]);
             }
         }
-        
     }
 
+    void addToTrie(String trie, String givenString) {
+        Trie toAdd = trieMap.get(trie);
+        if (trie.equals("B")) {
+            StringBuilder sb = new StringBuilder(givenString);
+            sb.reverse();
+            givenString = sb.toString();
+        }
 
+        for (int i = 0; i < givenString.length(); i++) {
+            if (toAdd.child.containsKey(givenString.charAt(i))) {
+                toAdd = toAdd.child.get(givenString.charAt(i));
+                toAdd.count += 1;
+            } else {
+                toAdd.child.put(givenString.charAt(i), new Trie(givenString.charAt(i)));
+                toAdd = toAdd.child.get(givenString.charAt(i));
+            }
+        }
+    }
+
+    void findString(String toFind) {
+        int toPrint = 0;
+        int[] aCounts = new int[toFind.length()];
+        int[] bCounts = new int[toFind.length()];
+        Trie findA = A;
+        Trie findB = B;
+        int aIndex = 0;
+        int bIndex = toFind.length() - 1;
+        while (aIndex < toFind.length()-1) {
+            if (findA.child.containsKey(toFind.charAt(aIndex))) {
+                findA = findA.child.get(toFind.charAt(aIndex));
+                if (findA.count == 0) {
+                    break;
+                }
+            } else {
+                break;
+            }
+            aCounts[aIndex] = findA.count;
+//            if (aIndex == i) {
+//                aCount = findA.count;
+//            }
+            aIndex += 1;
+        }
+        while (bIndex > 0) {
+            if (findB.child.containsKey(toFind.charAt(bIndex))) {
+                findB = findB.child.get(toFind.charAt(bIndex));
+                if (findB.count == 0) {
+                    break;
+                }
+            } else {
+                break;
+            }
+            bCounts[bIndex] = findB.count;
+//            if (bIndex == i + 1) {
+//                bCount = findB.count;
+//            }
+            bIndex -= 1;
+        }
+        for (int i = 0; i < toFind.length() - 1; i++) {
+            toPrint += aCounts[i] * bCounts[i + 1];
+        }
+//        for (int i = 0; i < toFind.length() - 1; i++) {
+//            int aCount = 0;
+//            int bCount = 0;
+//            
+//            
+//            if (aCount == 0) {
+//                continue;
+//            }
+//           
+//            toPrint += aCount * bCount;
+//        }
+        System.out.println(toPrint);
+    }
+
+    void deleteString(String trie, String givenString) {
+        Trie toDelete = trieMap.get(trie);
+        if (trie.equals("B")) {
+            StringBuilder sb = new StringBuilder(givenString);
+            sb.reverse();
+            givenString = sb.toString();
+        }
+
+        for (int i = 0; i < givenString.length(); i++) {
+            toDelete = toDelete.child.get(givenString.charAt(i));
+            toDelete.count -= 1;
+        }
+    }
+
+    static class Trie {
+        char key;
+        HashMap<Character, Trie> child;
+        int count;
+
+        public Trie() {
+            this.child = new HashMap<>();
+        }
+
+        public Trie(char key) {
+            this.key = key;
+            this.child = new HashMap<>();
+            this.count = 1;
+        }
+
+    }
 }
