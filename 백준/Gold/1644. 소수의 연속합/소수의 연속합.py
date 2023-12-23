a7ff4d24896692aa74546c9792be9c992378add7
DongJun Kim <djkim35417@gmail.com>
2023-12-23 17:26:22 +0900

    [Gold III] Title: 소수의 연속합, Time: 6964 ms, Memory: 143304 KB -BaekjoonHub

---
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.py
@@ -0,0 +1,31 @@
+n = int(input())
+
+count = 0
+primes = []
+for i in range(2, n + 1):
+    check = True
+    for j in range(2, int(i ** 0.5) + 1):
+        if i % j == 0:
+            check = False
+            break
+    if check:
+        primes.append(i)
+if primes:
+    left, right = 0, 1
+
+
+
+    while left <= right and right <= len(primes):
+        tempsum = sum(primes[left:right])
+        if tempsum == n:
+            count += 1
+            right += 1
+
+            
+        elif tempsum < n:
+            right += 1
+        else:
+            left += 1
+    print(count)
+else:
+    print(0)
