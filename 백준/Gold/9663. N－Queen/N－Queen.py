d85fb7d3252b38641c38c22377c560d6451faf77
DongJun Kim <djkim35417@gmail.com>
2024-02-19 12:52:46 +0900

    [Gold IV] Title: N-Queen, Time: 29740 ms, Memory: 213480 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,24 @@
+import sys
+n = int(sys.stdin.readline())
+result = 0
+cb = [0 for _ in range(n)]
+
+def check(m):
+    for i in range(m):
+        if cb[m] == cb[i] or abs(cb[i]-cb[m]) == m-i:
+            return False
+    return True
+
+def backtrack(v):
+    global result
+    if v == n:
+        result += 1
+        return
+    for i in range(n):
+        cb[v] = i
+        if check(v):
+            backtrack(v+1)
+
+backtrack(0)
+print(result)
+    
