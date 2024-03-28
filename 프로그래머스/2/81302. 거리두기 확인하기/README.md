721b3ab8a9b7ed3e7e4f7b4da1e323a9376212f2
DongJun Kim <djkim35417@gmail.com>
2024-03-28 10:17:47 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.05 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/README.md
+++ 프로그래머스/2/81302. 거리두기 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 거리두기 확인하기 - 81302 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81302) 
 
 ### 성능 요약
 
-메모리: 76.7 MB, 시간: 0.06 ms
+메모리: 74.7 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 03일 12:32:28
+2024년 03월 28일 10:17:45
 
 ### 문제 설명
 
--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -3,29 +3,25 @@ import java.util.*;
 class Solution {
     static final int[] DX = {0, 1, 1, 1, 0, -1, -1, -1};
     static final int[] DY = {1, 1, 0, -1, -1, -1, 0, 1};
-    
+
     static final char EMPTY = 'O';
     static final char PARTITION = 'X';
     static final char PERSON = 'P';
-    
+
     int[] answer;
     int n, m;
     char[][] places;
-    
+
     public int[] solution(String[][] places) {
-        List<Integer> answerList = new ArrayList<>();
-        for (String[] place : places) {
-            init(place);
-            answerList.add(solve());
-            solve();
-        } 
-        answer = new int[answerList.size()];
-        for (int i = 0; i < answerList.size(); i++) {
-            answer[i] = answerList.get(i);
+
+        answer = new int[5];
+        for (int i = 0; i < 5; i++) {
+            init(places[i]);
+            answer[i] = solve();
         }
         return answer;
     }
-    
+
     void init(String[] places) {
         n = 5;
         m = 5;
@@ -36,60 +32,64 @@ class Solution {
             }
         }
     }
-    
+
     int solve() {
-        int answer = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (places[i][j] == PERSON) {
-                    for (int d = 0; d < 8; d++) {
-                        if (d % 2 == 0) {
-                            int oneX = i + DX[d];
-                            int oneY = j + DY[d];
-                            if (isInner(oneX, oneY) && places[oneX][oneY] == PERSON) {
-                                return 0;
-                            }
-                            int twoX = i + DX[d]*2;
-                            int twoY = j + DY[d]*2;
-                            if (isInner(twoX, twoY) && places[twoX][twoY] == PERSON && places[oneX][oneY] == EMPTY) {
-                                return 0;
-                            }
-                            
-                        } else {
-                            int dx = i + DX[d];
-                            int dy = j + DY[d];
-                            if (isInner(dx, dy)) {
-                                int left = getLeft(d);
-                                int right = getRight(d);
-                                if (places[dx][dy] == PERSON) {
-                                    int leftX = i + DX[left];
-                                    int leftY = j + DY[left];
-                                    if (places[leftX][leftY] == EMPTY) {
-                                        return 0;
-                                    }
-                                    int rightX = i + DX[right];
-                                    int rightY = j + DY[right];
-                                    if (places[rightX][rightY] == EMPTY) {
-                                        return 0;
-                                    }
-                                }
-                            }
+                if (places[i][j] == PERSON && !checkPartition(i, j)) {
+                    return 0;
+                }
+            }
+        }
+        return 1;
+    }
+
+    boolean checkPartition(int i, int j) {
+        for (int d = 0; d < 8; d++) {
+            if (d % 2 == 0) {
+                int oneX = i + DX[d];
+                int oneY = j + DY[d];
+                if (isInner(oneX, oneY) && places[oneX][oneY] == PERSON) {
+                    return false;
+                }
+                int twoX = i + DX[d]*2;
+                int twoY = j + DY[d]*2;
+                if (isInner(twoX, twoY) && places[twoX][twoY] == PERSON && places[oneX][oneY] == EMPTY) {
+                    return false;
+                }
+
+            } else {
+                int dx = i + DX[d];
+                int dy = j + DY[d];
+                if (isInner(dx, dy)) {
+                    int left = getLeft(d);
+                    int right = getRight(d);
+                    if (places[dx][dy] == PERSON) {
+                        int leftX = i + DX[left];
+                        int leftY = j + DY[left];
+                        if (places[leftX][leftY] == EMPTY) {
+                            return false;
+                        }
+                        int rightX = i + DX[right];
+                        int rightY = j + DY[right];
+                        if (places[rightX][rightY] == EMPTY) {
+                            return false;
                         }
                     }
                 }
             }
         }
-        return answer;
+        return true;
     }
-    
+
     boolean isInner(int x, int y) {
         return 0 <= x && x < n && 0 <= y && y < m;
     }
-    
+
     int getLeft(int d) {
         return (d + 7) % 8;
     }
-    
+
     int getRight(int d) {
         return (d + 1) % 8;
     }
